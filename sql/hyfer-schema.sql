6c137b9efd5080d17376ba1d18ed473504fea4ac
Jim Cramer <remarcmij@gmail.com>
2018-08-01 22:25:24 +0200

    Update database schema to use utf8mb4 charset (emojis)

--- sql/hyfer-schema.sql
+++ sql/hyfer-schema.sql
@@ -5,9 +5,9 @@
 # http://www.sequelpro.com/
 # https://github.com/sequelpro/sequelpro
 #
-# Host: palulu (MySQL 5.7.22)
-# Database: hyfer
-# Generation Time: 2018-07-14 13:05:32 +0000
+# Host: eu-cdbr-west-02.cleardb.net (MySQL 5.6.38-log)
+# Database: heroku_6c6605327430ce5
+# Generation Time: 2018-08-01 20:24:09 +0000
 # ************************************************************
 
 
@@ -23,19 +23,23 @@
 # Dump of table events
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `events`;
+
 CREATE TABLE `events` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `event_name` varchar(32) NOT NULL DEFAULT '',
+  `event_name` varchar(32) COLLATE utf8mb4_bin NOT NULL DEFAULT '',
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `username` varchar(32) NOT NULL DEFAULT '',
-  `notes` varchar(255) DEFAULT NULL,
+  `username` varchar(32) COLLATE utf8mb4_bin NOT NULL DEFAULT '',
+  `notes` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 
 
 # Dump of table group_students
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `group_students`;
 
 CREATE TABLE `group_students` (
   `group_id` int(8) NOT NULL,
@@ -44,88 +48,100 @@ CREATE TABLE `group_students` (
   KEY `fk_group_id` (`group_id`),
   CONSTRAINT `fk_group_id` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 
 
 # Dump of table groups
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `groups`;
+
 CREATE TABLE `groups` (
   `id` int(8) NOT NULL AUTO_INCREMENT,
-  `group_name` varchar(50) NOT NULL DEFAULT '',
+  `group_name` varchar(50) COLLATE utf8mb4_bin NOT NULL DEFAULT '',
   `starting_date` date NOT NULL,
   `archived` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `group_name` (`group_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 
 
 # Dump of table homework_assignments
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `homework_assignments`;
+
 CREATE TABLE `homework_assignments` (
   `id` int(8) NOT NULL AUTO_INCREMENT,
   `group_id` int(8) NOT NULL,
   `module_id` int(8) NOT NULL,
-  `title` varchar(255) NOT NULL DEFAULT '',
-  `assignment_link` varchar(255) NOT NULL DEFAULT '',
+  `title` varchar(255) COLLATE utf8mb4_bin NOT NULL DEFAULT '',
+  `assignment_link` varchar(255) COLLATE utf8mb4_bin NOT NULL DEFAULT '',
   `deadline` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 
 
 # Dump of table homework_reviews
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `homework_reviews`;
+
 CREATE TABLE `homework_reviews` (
   `id` int(8) NOT NULL AUTO_INCREMENT,
   `submission_id` int(8) NOT NULL,
   `reviewer_id` int(8) NOT NULL,
-  `comments` text NOT NULL,
+  `comments` mediumtext COLLATE utf8mb4_bin NOT NULL,
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 
 
 # Dump of table homework_submissions
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `homework_submissions`;
+
 CREATE TABLE `homework_submissions` (
   `id` int(8) NOT NULL AUTO_INCREMENT,
   `assignment_id` int(8) NOT NULL,
   `submitter_id` int(8) NOT NULL,
-  `github_link` varchar(255) NOT NULL DEFAULT '',
+  `github_link` varchar(255) COLLATE utf8mb4_bin NOT NULL DEFAULT '',
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `reviewer` varchar(255) DEFAULT NULL,
+  `reviewer` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 
 
 # Dump of table modules
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `modules`;
+
 CREATE TABLE `modules` (
   `id` int(8) NOT NULL AUTO_INCREMENT,
-  `module_name` varchar(50) NOT NULL DEFAULT '',
+  `module_name` varchar(50) COLLATE utf8mb4_bin NOT NULL DEFAULT '',
   `added_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `default_duration` int(2) NOT NULL DEFAULT '3',
   `sort_order` int(4) DEFAULT NULL,
-  `git_repo` varchar(50) DEFAULT NULL,
-  `color` varchar(8) DEFAULT NULL,
+  `git_repo` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `color` varchar(8) COLLATE utf8mb4_bin DEFAULT NULL,
   `optional` tinyint(1) DEFAULT '0',
   `has_homework` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 
 
 # Dump of table running_module_teachers
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `running_module_teachers`;
 
 CREATE TABLE `running_module_teachers` (
   `running_module_id` int(11) NOT NULL,
@@ -134,12 +150,14 @@ CREATE TABLE `running_module_teachers` (
   KEY `fk_user_id_2` (`user_id`),
   CONSTRAINT `fk_running_module_id` FOREIGN KEY (`running_module_id`) REFERENCES `running_modules` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_user_id_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 
 
 # Dump of table running_modules
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `running_modules`;
 
 CREATE TABLE `running_modules` (
   `id` int(8) NOT NULL AUTO_INCREMENT,
@@ -147,18 +165,20 @@ CREATE TABLE `running_modules` (
   `group_id` int(8) NOT NULL,
   `duration` int(8) DEFAULT '3',
   `position` int(8) DEFAULT NULL,
-  `notes` text,
+  `notes` mediumtext COLLATE utf8mb4_bin,
   PRIMARY KEY (`id`),
   KEY `fk_module` (`module_id`),
   KEY `fk_group_name` (`group_id`),
   CONSTRAINT `fk_group_name` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_module` FOREIGN KEY (`module_id`) REFERENCES `modules` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 
 
 # Dump of table students_history
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `students_history`;
 
 CREATE TABLE `students_history` (
   `running_module_id` int(8) NOT NULL,
@@ -171,24 +191,27 @@ CREATE TABLE `students_history` (
   KEY `idx_running_module_id` (`running_module_id`),
   CONSTRAINT `sh_fk_running_module_id` FOREIGN KEY (`running_module_id`) REFERENCES `running_modules` (`id`) ON DELETE CASCADE,
   CONSTRAINT `sh_fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 
 
 # Dump of table users
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `users`;
+
 CREATE TABLE `users` (
   `id` int(8) NOT NULL AUTO_INCREMENT,
-  `username` varchar(32) DEFAULT '',
-  `full_name` varchar(145) DEFAULT NULL,
-  `role` varchar(32) DEFAULT NULL,
+  `username` varchar(32) COLLATE utf8mb4_bin DEFAULT '',
+  `full_name` varchar(145) COLLATE utf8mb4_bin DEFAULT NULL,
+  `role` varchar(32) COLLATE utf8mb4_bin DEFAULT NULL,
   `register_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `linkedin_username` varchar(32) DEFAULT NULL,
-  `email` varchar(32) DEFAULT NULL,
+  `linkedin_username` varchar(32) COLLATE utf8mb4_bin DEFAULT NULL,
+  `email` varchar(32) COLLATE utf8mb4_bin DEFAULT NULL,
+  `notes` mediumtext COLLATE utf8mb4_bin,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 
 
